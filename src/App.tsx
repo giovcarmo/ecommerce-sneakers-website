b971c6b10e5cfdd2b97575f115c218e4d0121eca
Giovanni Vittorazze do Carmo <gvittorazze@icloud.com>
2025-11-24 11:58:46 -0300

    .

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ function App() {
   return (
     <CartProvider>
       <ThemeProvider>
-        <BrowserRouter basename="/ecomerce-website-sneakers">
+        <BrowserRouter basename="/ecomerce-website-sneakers/">
           <ECommerceHeader />
           <Routes>
             <Route path="/men" element={<MenProductsPage />} />
--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: 'ecomerce-website-sneakers',
+  base: '/ecomerce-website-sneakers/',
   plugins: [react(), tailwindcss()],
 })
